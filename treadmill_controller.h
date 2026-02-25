e6eaf79119bdf0e6558877736fabcb5ba39df419
Marc Sherry <msherry@gmail.com>
2026-02-24 17:43:08 -0800

    Initial treadmill controller
    Reads heart rate over BLE

---
+++ treadmill_controller.h
@@ -0,0 +1,8 @@
+#ifndef TREADMILL_CONTROLLER_H
+#define TREADMILL_CONTROLLER_H
+
+// Apps that publish HR data over BLE - for searching BLE advertise messages
+#define HEARTCAST "HeartCast - iPhon"
+#define ECHO "ECHO HR"
+
+#endif
