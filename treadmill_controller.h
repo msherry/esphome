2fc10e7e9f57f74dc73af84b9e30f0fbf56f007b
Marc Sherry <msherry@gmail.com>
2026-02-24 18:28:30 -0800

    HeartCast and Echo both work - heartcast is free, so switch to that

--- treadmill_controller.h
+++ treadmill_controller.h
@@ -2,7 +2,7 @@
 #define TREADMILL_CONTROLLER_H
 
 // Apps that publish HR data over BLE - for searching BLE advertise messages
-#define HEARTCAST "HeartCast - iPhon"
+#define HEARTCAST "HeartCast-iPhon"
 #define ECHO "ECHO HR"
 
 #endif
