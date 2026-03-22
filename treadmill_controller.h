117055c14d7ae05f8a19eab17527342355550d78
Marc Sherry <msherry@gmail.com>
2026-03-22 15:44:15 -0700

    Support Polar H10 via BLE

--- treadmill_controller.h
+++ treadmill_controller.h
@@ -1,7 +1,8 @@
 #ifndef TREADMILL_CONTROLLER_H
 #define TREADMILL_CONTROLLER_H
 
-// Apps that publish HR data over BLE - for searching BLE advertise messages
+// Apps/devices that publish HR data over BLE - for searching BLE advertise messages
+#define POLAR "Polar H10"
 #define HEARTCAST "HeartCast-iPhon"
 #define ECHO "ECHO HR"
 
